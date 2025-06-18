793545d30e264abf397235f0313967990803f5b6
Akikoke <sherbets9999@yahoo.co.jp>
2025-06-18 16:49:16 -0700

    commit4

--- app.py
+++ app.py
@@ -8,3 +8,12 @@ value = input("Enter your guess from 1 to 1000:")
 list = [500,200,100,100,50,20,10,10,5,0]
 min_value = 1
 max_value = 1000
+
+for i in list:
+    count += 1
+    if int(value) == x:
+        print("You got it! The hidden number is " + str(value))
+        print("It took you" + " " + str(count) + " guesses!")
+        break
+
+print("Good")
